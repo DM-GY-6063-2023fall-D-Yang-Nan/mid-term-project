f6f5a5520b9ce55ca6f1b7e7cc349a4304f9df91
NanYangEvelyn <ny2187@nyu.edu>
2023-10-13 17:30:59 -0400

    FIRST FINISH

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ let img;
 let leafX = 0;
 let numLeaf = 4;
 let space = 0;
+let v = 18;
+
+let originalV = 10;
 
 
 
@@ -12,24 +15,36 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  strokeWeight(0.5);
+  noFill();
 }
 
 function draw() {
   background(255);
   translate(width/2,height/2);
-//   for(let i = 0;i<10;i++){
-//     r = randomGaussian(0,width/4)
-//     arc(0,-height/4,r,r,random(0,360),random(0,360))
-// }
+  for (let i = 0; i < 10; i++) {
+    let r = randomGaussian(0, width / 4);
+    arc(0, -height / 4, r, r, random(0, 360), random(0, 360));
+  }
+  
+  let t = 255;
+  // background(255, 10);
 translate(-width/2,-height/2);
 for(let i = 0; i < numLeaf; i++){
-  let currentLeafX = leafX + i * 700;
-  image(img,currentLeafX,height-700,700,700);
+  let currentLeafX = leafX + i * 600;
+  image(img,currentLeafX,height-700,600,700);
   if(currentLeafX > width){
-   leafX -= 700;
+   leafX -= 600;
   }
 }
-leafX += 5;
+leafX += v;
+
+if(mouseIsPressed){
+  v = max(v - 0.1, 0);  // 减小v，但确保它不小于0
+  } else {
+    v = lerp(v, originalV, 0.1);  // 当鼠标没有被按下时，使v慢慢增加回到其原始值
+  }
 }
+
 
 
